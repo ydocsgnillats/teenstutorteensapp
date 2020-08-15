8fc0e7d0fdc2c5fa5b9691dc0915a4f3669042b5
Cody Stallings <cmstal0507@ung.edu>
2020-08-15 16:18:46 -0400

    style changes

--- assets/styles.js
+++ assets/styles.js
@@ -3,48 +3,104 @@ import { StyleSheet } from 'react-native'
 const styles = StyleSheet.create({
     container: {
         backgroundColor: 'white',
-        height: '100%'
+        height: '100%',
+        flex: 1,
     },
     img: {
         width: '100%',
         height: '25%',
         aspectRatio: 1,
-        right: 100
+        right: 100,
+        top: -24
     },
     bgImg: {
         flex:1,
         width: '100%',
         height: '100%'
     },
+    bigbutton: {
+        backgroundColor: '#5c79fe99',
+        width: '85%',
+        height: '85%',
+        top: -15,
+        justifyContent: 'center',
+        borderRadius: 6,
+        borderWidth: 2,
+        borderColor:'#5c79fe75'
+    },
+    bigbuttontext: {
+        textAlign: 'center',
+        fontSize: 35,
+        color: '#1e233b',
+        fontFamily: 'AmericanTypewriter'
+    },
     button: {
         margin: 10,
-        backgroundColor: 'grey',
-        width: '40%'
+        backgroundColor: '#5c79fe99',
+        width: '40%',
+        height: '50%',
+        justifyContent: 'center',
+        borderRadius: 6,
+        borderWidth: 2,
+        borderColor:'#5c79fe50'
+    },
+    socialButton:{
+        margin: 10,
+        justifyContent: 'center',
+        height: '15%',
+        top: -20,
+        borderRadius: 6,
+        borderWidth: 2,
+        backgroundColor: '#5c79fe99',
+        borderRadius: 6,
+        borderWidth: 2,
+        borderColor:'#5c79fe75'
     },
     buttonText: {
         textAlign: 'center',
-        padding: 15,
+        fontSize: 19,
+        padding: 5,
+        color: '#1e233b',
+        fontFamily: 'AmericanTypewriter'
+    },
+    inputStyle: {
+        top:-15,
+        borderColor: '#5c79fe',
+        borderWidth: 2,
+        margin:10,
+        padding:10,
+        fontSize: 20
+    },
+    inputStyleFind: {
+        borderColor: '#5c79fe',
+        borderWidth: 2,
+        margin:2,
+        padding:5,
+        width: '80%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        top:5,
+        left: 35,
         fontSize: 15
     },
-    ads: {
-        
-    },
-    inputArea: {
-        margin: 2,
-        padding: 5,
-        borderColor: 'grey',
-        borderWidth: 1,
-        width: '50%'
-    },
-    submitBtn: {
-        justifyContent: 'center',
+    submitStyle: {
         width: '50%',
-        height: '15%',
-        margin: 10,
+        height: '10%',
         backgroundColor: 'grey',
+        backgroundColor: '#5c79fe99',
+        borderRadius: 6,
+        borderWidth: 2,
+        borderColor:'#5c79fe75',
+        justifyContent:'center',
+        alignItems: 'center',
+        left: 85,
+        top: 10
     },
     home: {
-        top: 30,
+        top: 25,
+        justifyContent: 'center',
+        flex: 2,
+        flexDirection: 'row'
     },
     about: {
         justifyContent: 'center',
@@ -53,14 +109,13 @@ const styles = StyleSheet.create({
         color:'grey'
     },
     apply: {
-        justifyContent: 'center',
         top: 30,
         padding: 25
     },
     social: {
         justifyContent: 'center',
         top: 30,
-        padding: 25
+        padding: 10
     },
     sponsors: {
         justifyContent: 'center',
@@ -68,8 +123,7 @@ const styles = StyleSheet.create({
         padding: 25
     },
     find: {
-        justifyContent: 'center',
-        padding: 10
+        backgroundColor: 'white'
     }
 })
 
