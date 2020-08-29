c1da9dfe3146da171032cab41484bbbc21e057aa
Cody Stallings <cmstal0507@ung.edu>
2020-08-29 13:02:08 -0400

    added the nodejs backend, hosted through heroku

---
+++ backend/index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+const Client = require('emailjs')
+const bodyParser =  require('body-parser')
+
+const client = new Client.SMTPClient({
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    ssl: true,
+})
+
+let jsonParser = bodyParser.json()
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(bodyParser.json({ type: 'application/*+json' }))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .post('/find', jsonParser, (req, res) => {
+        client.send(
+        {
+                text: "First: " + req.body.first + "\nLast: " + req.body.last + "\nPhone: " + req.body.phone + "\nSubject: " + req.body.subject + "\nGrade: " + req.body.grade + "\nAge: " + req.body.age +$
+                from: 'TeensTutorTeens-iOS',
+                to: '<test@gmail.com>',
+                subject: 'New Student Signup!'
+        },
+        (err, message) => {
+                console.log(err || message)
+        })})
+  .post('/apply', jsonParser, (req, res) => {
+        client.send(
+        {
+                text: "First: " + req.body.first + "\nLast: " + req.body.last + "\nEmail: " + req.body.email + "\nWhere did you hear about us? " + req.body.where + "\nAnything else? " + req.body.anything,
+                from: 'TeensTutorTeens-iOS',       
+                to: '<test@gmail.com>',
+                subject: 'New Tutor Signup!'
+        },
+        (err, message) => {
+                console.log(err || message)
+        })
+        client.send(
+        {
+                text: 'Thank you for applying to be a tutor, ' + req.body.first + '! Please reply to this email with your resume attached, and we will get back to you soon!',
+                from: '<test@gmail.com>',
+                to: req.body.email,
+                subject: 'RE: Signup to be a tutor'
+        },
+        (err, message) => {
+                console.log(err || message)
+        })})
+
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
